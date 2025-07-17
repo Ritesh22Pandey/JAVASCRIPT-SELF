ba76061bb183f37014440568568f934a41ec927d
RITESH KUMAR PANDEY <135226854+Ritesh22Pandey@users.noreply.github.com>
2025-07-17 18:21:03 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12235
+let accountEmail = "ritesh@gmail.com"
+var accountPassword = "1236644"
+accountCity = "Varanasi"
+let accountStates
+
+// accountId = 23 // Not Allowed
+
+accountEmail = "hcjkl@gamil.com"
+accountPassword = "225468"
+accountCity = "Bangalore"
+
+console.log(accountId)
+
+/*
+PREFER NOT TO USE VAR
+-BECAUSE OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStates])
