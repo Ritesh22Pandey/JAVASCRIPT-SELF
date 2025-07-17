3a194efbdbe694bacf51ae8d230c7d240011015d
RITESH KUMAR PANDEY <135226854+Ritesh22Pandey@users.noreply.github.com>
2025-07-18 00:56:36 +0530

    commit all

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Ritesh");
+//code readability should be high
+
+let name ="ritesh"
+let age = 29
+let isLoggedIn = false
+let state;
+
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => undefined
+//symbol -while talking about unique -- will talk about it while teaching React
+
+//object
+
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined 
